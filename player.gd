8b0a7c07f44fac38e874f9e589729b7553e94095
444B <15311579+444B@users.noreply.github.com>
2024-03-14 20:51:42 +0200

    test
    test

--- player.gd
+++ player.gd
@@ -4,10 +4,13 @@ func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 850
 	move_and_slide()
-	print(velocity)
+	print(velocity.length())
 
 	if velocity.length() > 0.0:
 		get_node("HappyBoo").play_walk_animation()
 	else:
-		get_node("HappyBoo").play_idle_animation()
-	
+		$HappyBoo.play_idle_animation() # we can also call it like this
+		# If we assign a Unique name to the node
+		# we can call it with %HappyBoo
+		# and not worry about absolute pathing
+		
